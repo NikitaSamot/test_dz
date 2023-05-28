e37075de2bad0915dc9c60adc0abe058d9727fb9
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2023-05-28 19:07:32 +0500

    Контроллеры-классы

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+from .models import Item
 
 
 def home(request):
@@ -7,3 +9,33 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+class ItemView(View):
+    def get(self, request):
+        items = Item.objects.all()
+        return render(request, 'items.html', {'items': items})
+
+
+class CreateItemView(View):
+    def get(self, request):
+        return render(request, 'create_item.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        return redirect('items')
+
+
+class GroupView(View):
+    def get(self, request):
+        groups = Group.objects.all()
+        return render(request, 'groups.html', {'groups': groups})
+
+
+class CreateGroupView(View):
+    def get(self, request):
+        return render(request, 'create_group.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        return redirect('groups')
