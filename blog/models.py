08bbc1d290a1f03a186a4054f0b066ba69655f2c
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2023-05-31 05:00:20 +0500

    Домашнее задание № 29: Модуль 16. Модели: расширенные инструменты

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import Group, User
+
 # Create your models here.
 
 
@@ -14,3 +15,18 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+products = Product.objects.prefetch_related('category')
+# В результате использования метода prefetch_related() с указанием правильного имени поля можно
+# значительно повысить производительность и сократить количество запросов к базе данных при работе
+# с моделью, описывающей товар, и её связанными объектами или данными.
