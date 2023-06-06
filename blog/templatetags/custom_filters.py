44a4a7f75b6dd1aa4fe4f4307db1543d0b48b452
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2023-06-06 07:24:40 +0500

    Домашнее задание № 36: Модуль 19. Шаблоны: Расширенные инструменты

--- blog/templatetags/custom_filters.py
+++ blog/templatetags/custom_filters.py
@@ -1,9 +1,21 @@
 from django import template
-
+from datetime import date
 register = template.Library()
+from blog.models import MenuItem
 
 
 @register.filter
 def custom_filter(value):
     # Дополнить фильтр согласно вашим требованиям
     return value
+
+
+@register.filter
+def current_year():
+    return date.today().year
+
+
+@register.inclusion_tag('blog/menu.html')
+def menu_items():
+    items = MenuItem.objects.all()
+    return {'menu_items': items}
