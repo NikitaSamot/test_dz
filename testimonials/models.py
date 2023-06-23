a9ac23f5297b9ae959293f55fa4dcce17e0e08f8
NikitaSamot <110569446+NikitaSamot@users.noreply.github.com>
2023-06-23 21:20:11 +0500

    Домашнее задание № 1 2 Модуль 7 Выборка данных

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.CharField(max_length=50)
+
+
+# Выборка только части колонок
+products = Product.objects.values('name', 'price')
+# Исключение определенных значений
+excluded_products = Product.objects.exclude(category='Electronics')
